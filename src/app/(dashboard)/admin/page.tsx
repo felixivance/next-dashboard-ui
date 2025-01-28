d2f189793c79344a05d76382b6a9e5981866afa5
Felix <felixrunye@gmail.com>
2025-01-28 14:46:23 +0300

    updated page styling

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,5 @@
+import AttendanceChart from '@/components/AttendanceChart';
+import CountChart from '@/components/CountChart';
 import UserCard from '@/components/UserCard';
 import React from 'react';
 
@@ -7,7 +9,7 @@ const AdminPage = (props: Props) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* left */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className='flex gap-4 justify-between flex-wrap'>
           <UserCard type="students" />
           <UserCard type="teachers" />
@@ -15,6 +17,17 @@ const AdminPage = (props: Props) => {
           <UserCard type="staffs" />
 
         </div>
+        {/* charts */}
+        <div className='flex gap-4 flex-col lg:flex-row'>
+          <div className='w-full lg:w-1/3 h-[450px]'>
+          <CountChart />
+          </div>
+          {/* attendance chart */}
+          <div className='w-full lg:w-2/3 h-[450px]'>
+          <AttendanceChart />
+          </div>
+        </div>
+        {/* bottom charts */}
       </div>
       {/* right */}
       <div className="w-full lg:w-1/3">right</div>
