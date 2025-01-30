8321ae60cc35fcc3bdfee8d1b8c1c6db69718567
Felix <felixrunye@gmail.com>
2025-01-30 09:22:34 +0300

    performance graph

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,5 +1,7 @@
 import BigCalendar from '@/components/BigCalendar';
+import Performance from '@/components/Performance';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {};
@@ -22,11 +24,11 @@ const SingleTeacherPage = (props: Props) => {
             </div>
             <div className="flex flex-col gap-4 w-2/3 justify-between">
               <div>
-                <h1 className='text-xl font-semibold'>Felix Teacher</h1>
-                <p className='text-sm text-gray-500'> Lorem ipsum</p>
+                <h1 className="text-xl font-semibold">Felix Teacher</h1>
+                <p className="text-sm text-gray-500"> Lorem ipsum</p>
               </div>
-              <div className='flex items-center justify-between gap-2 flex-wrap text-xs font-medium'>
-              <div className="flex gap-2 items-center w-full md:w-1/3 lg:w-full 2xl:w-1/3">
+              <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                <div className="flex gap-2 items-center w-full md:w-1/3 lg:w-full 2xl:w-1/3">
                   <Image src="/blood.png" alt="" width={14} height={14} />
                   <p>A+</p>
                 </div>
@@ -49,47 +51,92 @@ const SingleTeacherPage = (props: Props) => {
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
             {/* card */}
             <div className="bg-white p-4 rounded-md flex items-center gap-2 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                <Image src="/singleAttendance.png" alt="" width={24} height={24} className='w-6 h-6' />
-                <div className="">
-                    <h1 className='text-xl font-semibold'>90%</h1>
-                    <span className='text-sm'>Attendance</span>
-                </div>
+              <Image
+                src="/singleAttendance.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">90%</h1>
+                <span className="text-sm">Attendance</span>
+              </div>
             </div>
 
             <div className="bg-white p-4 rounded-md flex items-center gap-2 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                <Image src="/singleBranch.png" alt="" width={24} height={24} className='w-6 h-6' />
-                <div className="">
-                    <h1 className='text-xl font-semibold'>2%</h1>
-                    <span className='text-sm text-gray-400'>Branches</span>
-                </div>
+              <Image
+                src="/singleBranch.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">2%</h1>
+                <span className="text-sm text-gray-400">Branches</span>
+              </div>
             </div>
 
             <div className="bg-white p-4 rounded-md flex items-center gap-2 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                <Image src="/singleLesson.png" alt="" width={24} height={24} className='w-6 h-6' />
-                <div className="">
-                    <h1 className='text-xl font-semibold'>6%</h1>
-                    <span className='text-sm'>Lessons</span>
-                </div>
+              <Image
+                src="/singleLesson.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6%</h1>
+                <span className="text-sm">Lessons</span>
+              </div>
             </div>
 
             <div className="bg-white p-4 rounded-md flex items-center gap-2 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
-                <Image src="/singleClass.png" alt="" width={24} height={24} className='w-6 h-6' />
-                <div className="">
-                    <h1 className='text-xl font-semibold'>6</h1>
-                    <span className='text-sm'>Classes</span>
-                </div>
+              <Image
+                src="/singleClass.png"
+                alt=""
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="">
+                <h1 className="text-xl font-semibold">6</h1>
+                <span className="text-sm">Classes</span>
+              </div>
             </div>
-
           </div>
         </div>
         {/* bottom */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
-            <h1>Teachers Schedule</h1>
-            <BigCalendar />
+          <h1>Teachers Schedule</h1>
+          <BigCalendar />
         </div>
       </div>
       {/* right */}
-      <div className="w-full xl:w-1/3 bg-red-500"></div>
+      <div className="w-full xl:w-1/3">
+        <div className="bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Shortcuts</h1>
+          <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
+            <Link className="p-3 rounded-md bg-skyLight" href="/">
+              Teacher&apos;s Classes
+            </Link>
+            <Link className="p-3 rounded-md bg-purpleLight" href="/">
+              Teacher&apos;s Students
+            </Link>
+            <Link className="p-3 rounded-md bg-yellowLight" href="/">
+              Teacher&apos;s Lessons
+            </Link>
+            <Link className="p-3 rounded-md bg-pink-50" href="/">
+              Teacher&apos;s Exams
+            </Link>
+            <Link className="p-3 rounded-md bg-skyLight" href="/">
+              Teacher&apos;s Assignments
+            </Link>
+          </div>
+        </div>
+        <Performance />
+      </div>
     </div>
   );
 };
