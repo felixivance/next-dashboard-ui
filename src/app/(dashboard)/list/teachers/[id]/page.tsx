fb6fc49aadc96d4a63b3aa1e510d1d0453d8c30b
Felix <felixrunye@gmail.com>
2025-01-30 16:27:14 +0300

    student list page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from '@/components/Announcements';
 import BigCalendar from '@/components/BigCalendar';
 import Performance from '@/components/Performance';
 import Image from 'next/image';
@@ -114,7 +115,7 @@ const SingleTeacherPage = (props: Props) => {
         </div>
       </div>
       {/* right */}
-      <div className="w-full xl:w-1/3">
+      <div className="w-full xl:w-1/3 flex flex-col gap-4">
         <div className="bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Shortcuts</h1>
           <div className="mt-4 flex gap-4 flex-wrap text-xs text-gray-500">
@@ -136,6 +137,7 @@ const SingleTeacherPage = (props: Props) => {
           </div>
         </div>
         <Performance />
+        <Announcements/>
       </div>
     </div>
   );
