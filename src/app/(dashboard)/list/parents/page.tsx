b43c4c015408de52dada09bc2e2d020fa9e2f34b
Felix <felixrunye@gmail.com>
2025-01-30 17:23:13 +0300

    forms for creating teacher and student

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,85 +1,69 @@
-import Pagination from '@/components/Pagination';
-import Table from '@/components/Table';
-import TableSearch from '@/components/TableSearch';
-import { parentsData, role, studentsData, teachersData } from '@/lib/data';
-import { Column, Parent, Student, Teacher } from '@/lib/dataTypes';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
+import FormModal from "@/components/FormModal";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { parentsData, role } from "@/lib/data";
+import { Parent } from "@/lib/dataTypes";
+import Image from "next/image";
 
-type Props = {};
-
-const columns: Column[] = [
+const columns = [
   {
-    header: 'Id',
-    accessor: 'id',
-    className: 'hidden md:table-cell',
+    header: "Info",
+    accessor: "info",
   },
   {
-    header: 'Info',
-    accessor: 'info',
-  },
-
-  {
-    header: 'Students',
-    accessor: 'students',
-    className: 'hidden md:table-cell',
-  },
-  
-  {
-    header: 'Phone',
-    accessor: 'phone',
-    className: 'hidden md:table-cell',
+    header: "Student Names",
+    accessor: "students",
+    className: "hidden md:table-cell",
   },
   {
-    header: 'Address',
-    accessor: 'address',
-    className: 'hidden lg:table-cell',
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
   },
   {
-    header: 'Actions',
-    accessor: 'action',
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
   },
 ];
 
-const ParentsListPage = (props: Props) => {
-  const renderRow = (item: Parent) => {
-    return (
-      <tr
-        key={item.id}
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
-      >
-        <td className="hidden md:table-cell">{item.id}</td>
-        <td className="flex items-center gap-4 p-4">
-          <div className="flex flex-col">
-            <h3 className="font-semibold">{item.name}</h3>
-            <p className="text-xs text-gray-500">{item?.email}</p>
-          </div>
-        </td>
-        <td className="hidden md:table-cell">{item.students.join(",")}</td>
-        <td className="hidden md:table-cell">{item.phone}</td>
-        <td className="hidden md:table-cell">{item.address}</td>
-        <td>
-          <div className="flex items-center gap-2">
-            <Link href={`/list/teachers/${item.id}`}>
-              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
-                <Image src="/edit.png" alt="" width={16} height={16} />
-              </button>
-            </Link>
-            {role === 'admin' && (
-              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple">
-                <Image src="/delete.png" alt="" width={16} height={16} />
-              </button>
-            )}
-          </div>
-        </td>
-      </tr>
-    );
-  };
+const ParentListPage = () => {
+  const renderRow = (item: Parent) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.students.join(",")}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" && (
+            <>
+              <FormModal table="parent" type="update" data={item} />
+              <FormModal table="parent" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/* top */}
-      <div className="flex justify-between">
+      {/* TOP */}
+      <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
@@ -90,20 +74,18 @@ const ParentsListPage = (props: Props) => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button>
+            {role === "admin" && (
+              <FormModal table="parent" type="create"/>
+            )}
           </div>
         </div>
       </div>
-      {/* list */}
-      <div className="">
-        <Table columns={columns} renderRow={renderRow} data={parentsData} />
-      </div>
-      {/* pagination */}
+      {/* LIST */}
+      <Table columns={columns} renderRow={renderRow} data={parentsData} />
+      {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default ParentsListPage;
+export default ParentListPage;
